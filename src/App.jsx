9dc436c8e0fcea157e8e4575505fd5591a9b0b8a
janarbek <janarbekoljobaev55@gmail.com>
2024-09-07 22:23:17 +0300

    Finished HW-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,61 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <section>
+      <h1>Трикотажное платье</h1>
+      <img src="https://shorturl.at/2PnZG" alt="" />
+      <div className="container">
+        <div className="companent">
+          <p className="parograft">Цвета в наличии</p>
+          <div className="coropca"></div>
+          <div className="coropca1"></div>
+          <div className="coropca2"></div>
+          <div className="coropca3"></div>
+        </div>
+        <div className="companent1">
+          <p>Размеры в наличии</p>
+          <p>XXS, XS, S, M, L, XL, XXL</p>
+        </div>
+      </div>
+
+      <div className="rabotaet">
+        <h1>Платье</h1>
+        <img src="https://shorturl.at/qADhz" alt="" />
+        <div className="container">
+          <div className="companent">
+            <p className="parograft">Цвета в наличии</p>
+            <div className="corop"></div>
+            <div className="corop1"></div>
+            <div className="corop2"></div>
+            <div className="corop3"></div>
+          </div>
+          <div className="companent1">
+            <p>Размеры в наличии</p>
+            <p>XXS, XS, S, M, L, XL, XXL</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="ishtedigo">
+        <h1>Вязанный топ</h1>
+        <img src="https://shorturl.at/FjKPc" alt="" />
+        <div className="container">
+          <div className="companent">
+            <p className="parograft">Цвета в наличии</p>
+            <div className="coropca"></div>
+            <div className="coropca1"></div>
+            <div className="coropca2"></div>
+            <div className="coropca3"></div>
+          </div>
+          <div className="companent1">
+            <p>Размеры в наличии</p>
+            <p>XXS, XS, S, M, L, XL, XXL</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
